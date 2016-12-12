034798e814ac8aea68fa10892e7803ee7c6b53dd
hangjw0322 <hangjw0322@thundersoft.com>
2016-12-12 14:49:42 +0800

    Initial Commit

---
+++ TSInterfaceOrientation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TSInterfaceOrientation
+//
+//  Created by huangjianwu on 2016/12/12.
+//  Copyright © 2016年 huangjianwu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TSInterfaceOrientation/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TSInterfaceOrientation
+//
+//  Created by huangjianwu on 2016/12/12.
+//  Copyright © 2016年 huangjianwu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
